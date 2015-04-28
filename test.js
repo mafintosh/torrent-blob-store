548202c922dee4c519bc1e10c64baeeee5e072cb
James Halliday <mail@substack.net>
2015-04-28 12:27:02 -0300

    use custom tracker

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 var test = require('tape')
+var Tracker = require('bittorrent-tracker').Server
 var blob = require('./')
 
 require('abstract-blob-store/tests')(test, {
   setup: function (t, cb) {
-    cb(null, blob())
+ 
+    var server = new Tracker({ http: true })
+    server.listen(0, '127.0.0.1', function () {
+      cb(null, blob({
+        trackers: [
+          'http://localhost:' + server.http.address().port,
+          'udp://localhost:' + server.udp.address().port
+        ]
+      }))
+    })
   },
   teardown: function (t, store, blob, cb) {
     if (blob) store.remove(blob)
